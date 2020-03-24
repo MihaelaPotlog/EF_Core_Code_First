f3e8e91e1874aa90a0641f88b569d7d39d020ceb
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2020-03-25 00:03:17 +0200

    EF Core code first

---
+++ ClassLibraryNetCore/ClassLibraryNetCore/OneEntity/PersonsContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibraryNetCore
+{
+    public sealed class PersonsContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public PersonsContext()
+        {
+            Database.EnsureCreated();
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(SqlDatabaseString.ClassLibraryNetCore);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().Property(person => person.FirstName).HasMaxLength(10).IsRequired();
+            modelBuilder.Entity<Person>().Property(person => person.LastName).HasMaxLength(10).IsRequired();
+            modelBuilder.Entity<Person>().Property(person => person.MiddleName).HasMaxLength(10).IsRequired();
+            modelBuilder.Entity<Person>().Property(person => person.TelephoneNumber).HasMaxLength(10).IsRequired();
+
+        }
+    }
+}
---
+++ ClassLibraryNetCore/ClassLibraryNetCore/OneToMany/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibraryNetCore.OneToMany
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal TotalValue { get; set; }
+        public DateTime Date { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
